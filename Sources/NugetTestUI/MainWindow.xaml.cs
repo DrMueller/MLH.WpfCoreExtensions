286d55dff92d8a79cdaa073c35023f9333460ca7
Matthias Müller <MatthiasM@Live.de>
2019-05-18 13:29:25 +0200

    WIP

---
+++ Sources/NugetTestUI/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Mmu.Mlh.WpfCoreExtensions.NugetTestUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
