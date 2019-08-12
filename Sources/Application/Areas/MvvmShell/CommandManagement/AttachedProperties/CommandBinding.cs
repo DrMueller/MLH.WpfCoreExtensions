d364182351f81d70c5376c00fc25d0eed804f83d
Matthias Müller <matthias.mueller@trivadis.com>
2019-08-12 08:20:31 +0200

    WIP

--- Sources/Application/Areas/MvvmShell/CommandManagement/AttachedProperties/CommandBinding.cs
+++ Sources/Application/Areas/MvvmShell/CommandManagement/AttachedProperties/CommandBinding.cs
@@ -8,36 +8,43 @@ namespace Mmu.Mlh.WpfCoreExtensions.Areas.MvvmShell.CommandManagement.AttachedPr
     public static class CommandBinding
     {
         public static readonly DependencyProperty ViewModelCommandProperty =
-            DependencyProperty
-            .RegisterAttached(
+            DependencyProperty.RegisterAttached(
                 "ViewModelCommand",
                 typeof(IViewModelCommand),
                 typeof(CommandBinding),
-                new PropertyMetadata(AttachOrRemoveBinding));
+                new PropertyMetadata(AttachBinding));
 
-        public static IViewModelCommand ViewModelCommand(DependencyObject dependencyObject)
-        {
-            return (IViewModelCommand)dependencyObject.GetValue(ViewModelCommandProperty);
-        }
-
-        public static void ViewModelCommand(DependencyObject dependencyObject, IViewModelCommand value)
-        {
-            dependencyObject.SetValue(ViewModelCommandProperty, value);
-        }
-
-        public static void AttachOrRemoveBinding(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        public static void AttachBinding(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
         {
             var viewModelCommand = (IViewModelCommand)args.NewValue;
 
             if (dependencyObject is ButtonBase buttonBase)
             {
-                buttonBase.Content = viewModelCommand.Description;
-                buttonBase.Command = viewModelCommand.Command;
+                if (viewModelCommand != null)
+                {
+                    buttonBase.Content = viewModelCommand.Description;
+                    buttonBase.Command = viewModelCommand.Command;
+                }
+                else
+                {
+                    buttonBase.Content = null;
+                    buttonBase.Command = null;
+                }
             }
             else
             {
                 throw new Exception("ViewModelCommand must implement ButtonBase.");
             }
+        }
+
+        public static IViewModelCommand GetViewModelCommand(DependencyObject dependencyObject)
+        {
+            return (IViewModelCommand)dependencyObject.GetValue(ViewModelCommandProperty);
+        }
+
+        public static void SetViewModelCommand(DependencyObject dependencyObject, IViewModelCommand value)
+        {
+            dependencyObject.SetValue(ViewModelCommandProperty, value);
         }
     }
 }
