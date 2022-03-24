f003290aa2654d13ee24bd311dc5620ce0c0b93c
Matthias Müller <matthias.mueller@trivadis.com>
2022-03-24 08:21:23 +0100

    WIP

--- Sources/Application/Areas/MvvmShell/ViewModels/ViewModelBase.cs
+++ Sources/Application/Areas/MvvmShell/ViewModels/ViewModelBase.cs
@@ -1,18 +1,34 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 namespace Mmu.Mlh.WpfCoreExtensions.Areas.MvvmShell.ViewModels
 {
+    [PublicAPI]
     public abstract class ViewModelBase : IViewModel, INotifyPropertyChanged
     {
+        private readonly IDictionary<string, Action> _propChangeCallbacks = new Dictionary<string, Action>();
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void PublishPropertyChanged(string propertyName)
         {
+            if (_propChangeCallbacks.TryGetValue(propertyName, out var callback))
+            {
+                callback();
+            }
+
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected virtual void OnPropertyChanged<T>(T newValue, ref T oldValue,
+        public void RegisterPropertyChange(string propertyName, Action callback)
+        {
+            _propChangeCallbacks.Add(propertyName, callback);
+        }
+
+        protected virtual void OnPropertyChanged<T>(
+            T newValue, ref T oldValue,
             [CallerMemberName] string propertyName = null)
         {
             if (Equals(newValue, oldValue))
