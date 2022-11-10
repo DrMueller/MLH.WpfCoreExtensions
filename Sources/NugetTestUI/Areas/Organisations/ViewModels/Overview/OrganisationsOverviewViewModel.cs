6892d99286b7045ecec42b20ae14a095a21cebaa
Matthias Mueller U810688 <Matthias.Mueller@mobi.ch>
2022-11-10 11:27:20 +0100

    WIP

--- Sources/NugetTestUI/Areas/Organisations/ViewModels/Overview/OrganisationsOverviewViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mmu.Mlh.WpfCoreExtensions.Areas.MvvmShell.ViewModels;
-using Mmu.Mlh.WpfCoreExtensions.Areas.MvvmShell.ViewModels.Behaviors;
-
-namespace Mmu.Mlh.WpfCoreExtensions.NugetTestUI.Areas.Organisations.ViewModels.Overview;
-
-public class OrganisationsOverviewViewModel : ViewModelBase, IDisplayableViewModel
-{
-    public string HeadingDescription => "Organisations Overview";
-}
